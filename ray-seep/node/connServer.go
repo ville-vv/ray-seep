e9701113b0fb21e9a7d3106bb20a33d45506d1bc
ville <songwei@mintechai.com>
2019-09-27 18:11:51 +0800

    add:pproxy

--- ray-seep/node/connServer.go
+++ ray-seep/node/connServer.go
@@ -41,7 +41,7 @@ func (sel *ConnServer) Start() error {
 	}
 	vlog.INFO("ConnServer start [%s]", sel.addr)
 	for c := range lis.Conn {
-		sel.dealConn(c)
+		go sel.dealConn(c)
 	}
 	return nil
 }
@@ -49,45 +49,44 @@ func (sel *ConnServer) Start() error {
 // dealConn 处理连接
 func (sel *ConnServer) dealConn(c conn.Conn) {
 	vlog.DEBUG("customer [%d] connecting ", c.Id())
-	go func() {
-		defer func() {
-			if r := recover(); r != nil {
-				vlog.LogE("customer listener failed with error %v: %s", r, debug.Stack())
-			}
-		}()
-		defer c.Close()
-		// 刚刚建立连接需要设置超时时间
-		_ = c.SetReadDeadline(time.Now().Add(sel.timeout))
-		msgMng := mng.NewMsgTransfer(c)
 
-		// 通知有用户连接
-		if err := sel.ish.OnConnect(c.Id(), msgMng); err != nil {
-			vlog.ERROR("customer [%d] connect fail %s", c.Id(), err.Error())
-			return
-		}
-		vlog.DEBUG("customer [%d] connect success", c.Id())
-		// 通知有连接断开
-		defer sel.ish.OnDisConnect(c.Id())
-		_ = c.SetReadDeadline(time.Time{})
-
-		wg := sync.WaitGroup{}
-		recvMsg := make(chan pkg.Package)
-		cancel := make(chan pkg.Package)
-		// 开启一个协程 接收消息
-		msgMng.RecvMsgWithChan(&wg, recvMsg, cancel)
-		wg.Wait()
-		for {
-			select {
-			case req := <-recvMsg:
-				rsp, err := sel.ish.OnMessage(c.Id(), &req)
-				if err != nil {
-					// 执行消息出现错误
-					rsp = pkg.Package{Cmd: pkg.CmdError, Body: []byte(err.Error())}
-				}
-				msgMng.SendMsg(&rsp)
-			case <-cancel:
-				return
-			}
+	defer func() {
+		if r := recover(); r != nil {
+			vlog.LogE("customer listener failed with error %v: %s", r, debug.Stack())
 		}
 	}()
+	defer c.Close()
+	// 刚刚建立连接需要设置超时时间
+	_ = c.SetReadDeadline(time.Now().Add(sel.timeout))
+	msgMng := mng.NewMsgTransfer(c)
+
+	// 通知有用户连接
+	if err := sel.ish.OnConnect(c.Id(), msgMng); err != nil {
+		vlog.ERROR("customer [%d] connect fail %s", c.Id(), err.Error())
+		return
+	}
+	vlog.DEBUG("customer [%d] connect success", c.Id())
+	// 通知有连接断开
+	defer sel.ish.OnDisConnect(c.Id())
+	_ = c.SetReadDeadline(time.Time{})
+
+	wg := sync.WaitGroup{}
+	recvMsg := make(chan pkg.Package)
+	cancel := make(chan pkg.Package)
+	// 开启一个协程 接收消息
+	msgMng.RecvMsgWithChan(&wg, recvMsg, cancel)
+	wg.Wait()
+	for {
+		select {
+		case req := <-recvMsg:
+			rsp, err := sel.ish.OnMessage(c.Id(), &req)
+			if err != nil {
+				// 执行消息出现错误
+				rsp = pkg.Package{Cmd: pkg.CmdError, Body: []byte(err.Error())}
+			}
+			msgMng.SendMsg(&rsp)
+		case <-cancel:
+			return
+		}
+	}
 }
--- ray-seep/server/proxy/proxy.go
+++ ray-seep/server/proxy/proxy.go
@@ -4,8 +4,18 @@
 // proxy
 package proxy
 
+import (
+	"errors"
+	"ray-seep/ray-seep/common/pkg"
+	"ray-seep/ray-seep/conn"
+	"ray-seep/ray-seep/mng"
+	"time"
+	"vilgo/vlog"
+)
+
 type Server struct {
-	addr string
+	addr      string
+	proxyConn chan conn.Conn
 }
 
 func NewServer() *Server {
@@ -14,5 +24,53 @@ func NewServer() *Server {
 	}
 }
 
-func Start() {
+func (s *Server) Start() {
+	ls, err := conn.Listen(s.addr)
+	if err != nil {
+		return
+	}
+	for c := range ls.Conn {
+		go s.dealConn(c)
+	}
+}
+
+func (s *Server) dealConn(cn conn.Conn) {
+	defer func() {
+		if err := recover(); err != nil {
+			vlog.DEBUG("")
+			return
+		}
+	}()
+	tr := mng.NewMsgTransfer(cn)
+	var regProxy pkg.Package
+
+	if err := tr.RecvMsg(&regProxy); err != nil {
+		cn.Close()
+		return
+	}
+
+	if regProxy.Cmd != pkg.CmdRegisterProxyReq {
+		cn.Close()
+		return
+	}
+}
+func (s *Server) SetProxy(cn conn.Conn) {
+	cn.SetDeadline(time.Now().Add(time.Second * 15))
+	select {
+	case s.proxyConn <- cn:
+	default:
+		vlog.WARN("Proxies buffer is full, discarding.")
+	}
+}
+
+func (s *Server) GetProxy() (cn conn.Conn, err error) {
+	var ok bool
+	select {
+	case cn, ok = <-s.proxyConn:
+		if !ok {
+			err = errors.New("No proxy connections available, control is closing")
+			return
+		}
+	}
+	return
 }
