5523c0db9736f27e4398524a1e5c94c9732bd1c3
ville <songwei@minteachai.com>
2019-11-26 18:39:32 +0800

    add:xxxx

--- ray-seep/common/repeat/repeater.go
+++ ray-seep/common/repeat/repeater.go
@@ -34,7 +34,7 @@ func (sel *NetRepeater) Transfer(host string, c net.Conn) {
 	}
 	defer pxyConn.Close()
 	_ = pxyConn.SetDeadline(time.Time{})
-	reqLength, respLength, err := sel.relay(pxyConn, c)
+	reqLength, respLength, err := Relay(pxyConn, c)
 	if err != nil {
 		if netErr, ok := err.(net.Error); !(ok && netErr.Timeout()) {
 			if err != io.EOF {
@@ -45,37 +45,39 @@ func (sel *NetRepeater) Transfer(host string, c net.Conn) {
 	vlog.INFO("request size：[%d]. response size：[%d]", reqLength, respLength)
 }
 
-// exchange 请求数据转播
+// pip
+
+type result struct {
+	N   int64
+	Err error
+}
+
+func relay(dst net.Conn, src net.Conn, resCh chan result) {
+	n, err := io.Copy(dst, src)
+	_ = src.SetDeadline(time.Now())
+	_ = dst.SetDeadline(time.Now())
+	resCh <- result{N: n, Err: err}
+}
+
+// Relay 请求数据转播
 // @dst 是目标请求网络连接
 // @src 是发起请求的连接
 // @return 1 : 请求者发送的数据长度
 // @return 2 : 被请求者返回的数据长度
 // @return err : 错误
-func (sel *NetRepeater) relay(dst net.Conn, src net.Conn) (int64, int64, error) {
-	type res struct {
-		N   int64
-		Err error
-	}
-	ch := make(chan res)
-	go func() {
-		// 先启动
-		n, err := io.Copy(src, dst)
-		_ = src.SetDeadline(time.Now())
-		_ = dst.SetDeadline(time.Now())
-		ch <- res{N: n, Err: err}
-
-	}()
+func Relay(dst net.Conn, src net.Conn) (int64, int64, error) {
+	rspCh := make(chan result)
+	// 返回数据转发
+	go relay(src, dst, rspCh)
 	n, err := io.Copy(dst, src)
 	_ = src.SetDeadline(time.Now())
 	_ = dst.SetDeadline(time.Now())
-	rs := <-ch
+	rs := <-rspCh
 	if err == nil {
 		err = rs.Err
 	}
 	return n, rs.N, err
 }
-
-// pip
 func (sel *NetRepeater) join(dst net.Conn, src net.Conn) (int64, int64, error) {
 	var wait sync.WaitGroup
 	var err error
