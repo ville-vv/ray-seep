a0ddcfdc998a9156800e10cab996fea0ed8a23fb
ville <vilsongwei@163.com>
2019-11-27 23:22:54 +0800

    add:redis

--- ray-seep/server/dao/redis/redis.go
+++ ray-seep/server/dao/redis/redis.go
@@ -1,9 +1,22 @@
 package redis
 
 import (
+	"fmt"
 	"github.com/go-redis/redis"
 )
 
 type Client struct {
-	redis.BitCount
+	rds *redis.Client
+}
+
+func NewClient(cli *redis.Client) *Client {
+	return &Client{
+		rds: cli,
+	}
+}
+
+func (c *Client) SetUserToken(user string, id int, token string) error {
+	key := fmt.Sprintf("%s_%d_token", user, id)
+	cmd := c.rds.Set(key, token, 0)
+	return cmd.Err()
 }
