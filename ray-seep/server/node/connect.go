92dd98c0d0f2fa867172c4c2a222510ca7eadf23
ville <songwei@minteachai.com>
2020-07-17 16:18:53 +0800

    update:管理pod map+mutex 改为 sync.Map

--- ray-seep/server/node/connect.go
+++ ray-seep/server/node/connect.go
@@ -9,13 +9,18 @@ import (
 	"ray-seep/ray-seep/server/hostsrv"
 	"ray-seep/ray-seep/server/ifc"
 	"sync"
+	"sync/atomic"
 )
 
+// 使用 sync.Map 代替 map+Mutex 因为pods读数据比较多，使用sync.Map更合适
+// pods 在客户端的代理链接断开后，如果有用户的访问请求，查询到register中没有client proxy 通道，
+// 这个时候 register 会调用 GetNotice 方法获取pod 发送数据，所以pods 会存在大量的 read操作
 type ConnectCenter struct {
-	mu    sync.Mutex
-	pods  map[int64]*Pod
+	mu sync.Mutex
+	//pods  map[int64]*Pod
+	pods  sync.Map
 	podHd ifc.PodHandler
-	cNum  int
+	cNum  int32
 	cfg   *conf.Server
 	hsr   hostsrv.HostServer
 	exit  ifc.ExitDevice
@@ -23,8 +28,8 @@ type ConnectCenter struct {
 
 func NewConnectCenter(cfg *conf.Server, runner hostsrv.HostServer, podHd ifc.PodHandler, exit ifc.ExitDevice) *ConnectCenter {
 	return &ConnectCenter{
-		mu:    sync.Mutex{},
-		pods:  make(map[int64]*Pod),
+		mu: sync.Mutex{},
+		//pods:  make(map[int64]*Pod),
 		podHd: podHd,
 		cNum:  0,
 		cfg:   cfg,
@@ -48,36 +53,30 @@ func (c *ConnectCenter) OnConnect(cancel chan interface{}, cn conn.Conn) error {
 func (c *ConnectCenter) OnDisConnect(id int64) {
 	// 认证成功加入到管理服务中
 	var name string
-	c.mu.Lock()
-	if pd, ok := c.pods[id]; ok {
+	if val, ok := c.pods.Load(id); ok {
+		pd := val.(*Pod)
 		name = pd.HttpAddr()
 		_ = pd.LogoutReq(nil)
-		delete(c.pods, id)
-		c.cNum--
+		atomic.AddInt32(&c.cNum, -1)
+		c.pods.Delete(id)
 	}
-	c.mu.Unlock()
 	c.exit.Logout(name, id)
 	vlog.DEBUG("[%d] disconnect current number:%d", id, c.cNum)
 }
 
 func (c *ConnectCenter) addPod(id int64, pd *Pod) error {
-	// 认证成功加入到管理服务中
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	c.pods[id] = pd
-	c.cNum += 1
+	atomic.AddInt32(&c.cNum, +1)
+	c.pods.Store(id, pd)
 	vlog.DEBUG("current client connect number [%d]", c.cNum)
 	return nil
 }
 
 func (c *ConnectCenter) GetNotice(id int64) (ifc.MessageNotice, error) {
-	c.mu.Lock()
-	pod, ok := c.pods[id]
+	val, ok := c.pods.Load(id)
 	if !ok {
-		c.mu.Unlock()
 		return nil, errs.ErrClientControlNotExist
 	}
-	c.mu.Unlock()
+	pod := val.(*Pod)
 	tmp := *pod
 	return &tmp, nil
 }
